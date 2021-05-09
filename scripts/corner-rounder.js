0d2c6f72804a88a6363e4253fe100e6f3ac313ef
Dario Freni <dario.freni@gmail.com>
2021-05-09 20:26:39 +0100

    Add bookmarklet to make all corners in the world rounder.

--- scripts/corner-rounder.js
+++ scripts/corner-rounder.js
@@ -1,3 +1,12 @@
 function cornerRounder() {
-	document.getElementById("mainheader").style.borderRadius= "50px";
+	// 1. get all the html elements with tag <div>
+	// 2. iterate over each element
+	//    a. change .style.borderRadius to 50px.
+
+	let elements = document.getElementsByTagName("*");
+	let index;
+	for (index = 0; index < elements.length; index++) {
+		let bit = elements[index];
+		bit.style.borderRadius="25px";
+	}
 }
